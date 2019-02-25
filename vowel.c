c9152067a523851a57ff524678664d07493e85f5
karthikeyan1998 <47741331+karthikeyan1998@users.noreply.github.com>
2019-02-25 16:37:21 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+	char a;
+	if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u'|a=='A'||a=='E'||a=='I'||a=='O'||a=='U')
+	printf("vowel");
+	else
+	printf("consonant");
+}
